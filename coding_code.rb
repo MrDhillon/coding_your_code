e3bfc1fcf622a6cc1dd5f52a541b1136c05789d9
MrDhillon <harman.singh.dhillon@gmail.com>
2015-08-25 17:03:59 -0400

    colors cost feature

--- coding_code.rb
+++ coding_code.rb
@@ -1,2 +1,14 @@
-def cost
+def cost(sqft,colors)
 end
+
+def colors(amount)
+    if amount <= 2
+        cpc = 10
+    elsif amount > 2
+        cpc = 15
+    end
+
+    return amount * cpc
+end
+
+cost(x,y)
